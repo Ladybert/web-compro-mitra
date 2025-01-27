f9103509c2993f16c6b8acc659f28ae3ae290bdb
FawzyCode <137638007+Ladybert@users.noreply.github.com>
2025-01-27 16:16:46 +0700

    second commit

---
+++ app/Views/template-user/user-content/section_one.php
@@ -0,0 +1,38 @@
+<section class="mt-64 flex flex-col relative">
+  <!-- Title -->
+  <div class="text-center text-[2.5rem]">
+    <h1 class="text-[#0C0A0A] font-semibold">Layanan Kami</h1>
+  </div>
+
+  <!-- Content -->
+  <div class="relative flex sm:flex-col md:flex-row items-start justify-center">
+    <!-- Left Side: Image -->
+    <div class="relative top-[-5rem] left-[-1rem] w-[50%]">
+      <img class="w-full" src="<?= base_url('assets/img/people.png') ?>" alt="Construction People">
+    </div>
+
+    <!-- Right Side: Service Grid -->
+    <div class="relative  left-[-1rem] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full md:w-3/4 place-content-start pt-20 pr-12">
+      <!-- Service 1 -->
+      <div class="flex items-center space-x-4 mb-12">
+        <img class="w-1/3" src="<?= base_url('assets/img/building-icon.png') ?>" alt="Jasa Pelaksana Konstruksi bangunan">
+        <p class="text-[#0C0A0A] font-medium text-2xl">Jasa<br/> Pelaksana<br/> Konstruksi<br/> Bangunan</p>
+      </div>
+      <!-- Service 2 -->
+      <div class="flex items-center space-x-4 mb-12">
+        <img class="w-1/3" src="<?= base_url('assets/img/gear-icon.png') ?>" alt="Jasa Pelaksana Mechanical dan Electrical">
+        <p class="text-[#0C0A0A] font-medium text-2xl">Jasa<br/> Pelaksana<br/> Mechanical<br/> dan Electrical</p>
+      </div>
+      <!-- Service 3 -->
+      <div class="flex items-center space-x-4 mb-12">
+        <img class="w-1/3" src="<?= base_url('assets/img/swimming-pool-icon.png') ?>" alt="Jasa Pelaksana Swimming Pool">
+        <p class="text-[#0C0A0A] font-medium text-2xl">Jasa<br/> Pelaksana<br/> Swimming<br/> Pool</p>
+      </div>
+      <!-- Service 4 -->
+      <div class="flex items-center space-x-4 mb-12">
+        <img class="w-1/3" src="<?= base_url('assets/img/highway-icon.png') ?>" alt="Jasa Pelaksana Asphalt Road">
+        <p class="text-[#0C0A0A] font-medium text-2xl">Jasa<br/> Pelaksana<br/> Asphalt<br/> Road</p>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ app/Views/template-user/user-content/section_two.php
@@ -0,0 +1,12 @@
+<section class="flex flex-col mb-24">
+    <div class="absolute top-[110vw] z-[-3] left-0 w-full h-[200vh] bg-[#1F3C88]">
+        <div class="flex flex-col justify-center items-center mt-24 text-center text-white">
+            <h1 class="font-semibold text-[2.5rem]">Portofolio Proyek</h1>
+            <p class="mt-4 font-medium text-[1rem] w-3/4">Proyek-proyek kami mencerminkan dedikasi terhadap kualitas dan inovasi. Dari konstruksi bangunan hingga instalasi mekanikal & elektrikal, kami siap mewujudkan visi Anda menjadi kenyataan.</p>
+        </div>
+        <div class="mt-12 grid gris-cols-1 md:grid-cols-2 text-white place-items-center">
+            <div>1</div>
+            <div>2</div>
+        </div>
+    </div>
+</section>
